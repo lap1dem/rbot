e2dcf87815e7ba156f0b010be2ff988b9e046e0f
lap1dem <vadym.bidula@gmail.com>
2023-02-17 23:57:51 -0500

    Server name fixed

--- r_bot/game_data/data_info.py
+++ r_bot/game_data/data_info.py
@@ -3,8 +3,11 @@ from .dbinit import data_conn
 
 @data_conn
 def table_exists(conn, cur, guild_name: str):
+    if '"' in guild_name:
+        print("Invalid guild name", guild_name)
+        return True
     query = f"""
-       SELECT FROM pg_tables WHERE schemaname='public' AND tablename='gamelist_{guild_name}'
+       SELECT FROM pg_tables WHERE schemaname="public" AND tablename="gamelist_{guild_name}"
        """
     cur.execute(query)
     res = cur.fetchall()
