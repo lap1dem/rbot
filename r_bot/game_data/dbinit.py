51a77df6fb2a6f2aa7e95ce6979878337ce45591
lap1dem <vadym.bidula@gmail.com>
2023-02-17 23:37:48 -0500

    Initial commit

--- r_bot/game_data/dbinit.py
+++ r_bot/game_data/dbinit.py
@@ -3,12 +3,12 @@ from urllib.parse import urlparse
 import redis
 import psycopg2 as psql
 
-# url = urlparse(os.environ.get("REDIS_URL"))
-# DATABASE_URL = os.environ['DATABASE_URL']
-url = urlparse("redis://:p74b9c7ec1b81e3384a5b6a77076443a17f34c2273316d001165144013def44b5@ec2-52-208-16-24.eu-west-1"
-               ".compute.amazonaws.com:26780")
-DATABASE_URL = "postgres://pbhzrbwqdhdasc:308079183f945c73cdd99f3444243ef2c5ff9e2f8543f6f7fcd91a51f1e7cc69@ec2-34-252" \
-               "-35-249.eu-west-1.compute.amazonaws.com:5432/db841kk2il7u68"
+url = urlparse(os.environ.get("REDIS_URL"))
+DATABASE_URL = os.environ['DATABASE_URL']
+# url = urlparse("redis://:p74b9c7ec1b81e3384a5b6a77076443a17f34c2273316d001165144013def44b5@ec2-52-208-16-24.eu-west-1"
+#                ".compute.amazonaws.com:26780")
+# DATABASE_URL = "postgres://pbhzrbwqdhdasc:308079183f945c73cdd99f3444243ef2c5ff9e2f8543f6f7fcd91a51f1e7cc69@ec2-34-252" \
+#                "-35-249.eu-west-1.compute.amazonaws.com:5432/db841kk2il7u68"
 
 RCONN = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=True, ssl_cert_reqs=None)
 
