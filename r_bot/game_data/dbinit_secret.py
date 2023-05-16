390ffc865a87ba2137a7e04d677ef81afcadbd4d
lap1dem <vadym.bidula@gmail.com>
2023-05-15 20:10:45 -0400

    .gitignore added

---
+++ r_bot/game_data/dbinit_secret.py
@@ -0,0 +1,5 @@
+from urllib.parse import urlparse
+redis_url = urlparse("redis://:p74b9c7ec1b81e3384a5b6a77076443a17f34c2273316d001165144013def44b5@ec2-52-208-16-24.eu-west-1"
+               ".compute.amazonaws.com:26780")
+DATABASE_URL = "postgres://pbhzrbwqdhdasc:308079183f945c73cdd99f3444243ef2c5ff9e2f8543f6f7fcd91a51f1e7cc69@ec2-34-252" \
+               "-35-249.eu-west-1.compute.amazonaws.com:5432/db841kk2il7u68"
